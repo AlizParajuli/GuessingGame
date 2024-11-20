22f95f346510a7b20d332708f7a00c3b73c0c560
Aliz <A00290934@mycambrian.ca>
2024-11-19 21:55:59 -0500

    Added readonly property

---
+++ GuessingGame/Guess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    public class Guess
+    {
+        public int UserGuess { get; }
+        public DateTime GuessTime { get; }
+
+    }
+}
