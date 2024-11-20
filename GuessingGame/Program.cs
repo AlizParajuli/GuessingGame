729824cfadb95cbffc9988e4ed5f6236c2a672c4
Aliz <A00290934@mycambrian.ca>
2024-11-19 22:01:00 -0500

    Implemented to create random number

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using GuessingGame;
+
+Random random = new Random();
+int randomNum = random.Next(1, 101);
+
+List<Guess> listGuesses = new List<Guess>();
+
+int guess = 0;
+
+
